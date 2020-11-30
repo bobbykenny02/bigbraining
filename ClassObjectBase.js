7301ff15df3c7ac5557d260bc9c4d75fd206381f
Glitch (hello-webpage) <none>
2020-11-30 20:27:49 +0000

    🌍🌯 Checkpoint ./ClassObjectBase.js:23289384/1099 ./GLTFLoder.js:23289384/96557 ./index.html:23289384/6998 ./page1.html:23289384/1778 ./PointerLockControls.js:23289384/3445 ./three.js:23289384/1231149 ./.glitch-assets:23289384/344

---
+++ ClassObjectBase.js
@@ -0,0 +1,37 @@
+class objectBase {
+    obj;
+    scene;
+    constructor(url,scene,scale) {
+        this.scene=scene;
+        //Create cube as a place holder
+        const geometry = new THREE.BoxGeometry();
+        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        this.obj = new THREE.Mesh(geometry, material);
+        
+        // Load from url 
+        const loader = new THREE.GLTFLoader();
+        loader.load(
+            "stool.glb",
+            (res) =>  {
+                var x= this.obj.position.x;
+                var y= this.obj.position.y;
+                var z= this.obj.position.z;
+                scene.remove(this.obj);
+                res.scene.scale.set(scale, scale, scale);    
+                this.obj = res.scene;  
+                this.obj.position.set(x,y,z);
+                scene.add(this.obj);         
+            }, undefined, function (error) {
+                console.error(error);
+            }
+        );        
+        scene.add(this.obj)
+    }
+    moveTo(x,y,z){
+        this.obj.position.set(x,y,z);
+    }
+    isJitter=0;
+    frameMove() {
+
+    }    
+}
